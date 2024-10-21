3b438616c3d815ffca738b98dce684efeee3a6fa
Pinto <pintokevin2002@hotmail.com>
2024-10-21 12:06:34 +0200

    Creation of the route to use the error page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Navigation from "./components/Navigation.jsx";
 import Home from "./pages/Home.jsx";
 import Footer from "./components/Footer.jsx";
+import Redirect from "./pages/Redirect.jsx";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
           <Navigation />
           <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/*" element={<Redirect />} />
           </Routes>
           <Footer />
       </BrowserRouter>
