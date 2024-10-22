0a6f23d03a641d653b8823df9257a7229d757a44
Pinto <pintokevin2002@hotmail.com>
2024-10-22 09:33:31 +0200

    Adding backdrop-filter blur to the navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import NavList from "./NavList.jsx";
 
 const Navigation = () => {
     return (
-        <Navbar className="mx-auto max-w-screen-xl px-6 py-3 sticky top-0 z-10">
+        <Navbar className="mx-auto max-w-screen-xl px-6 py-3 sticky top-0 z-10 backdrop-blur-md">
             <div className="flex items-center justify-between text-blue-gray-900">
                 <Typography
                     as="a"
