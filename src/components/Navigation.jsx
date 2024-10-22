ff943c6ab44141af5813425035d5be83913e9bb2
Pinto <pintokevin2002@hotmail.com>
2024-10-22 13:45:57 +0200

    Adding logic so the navbar changed base on if we're connected or no

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,11 @@
 import {Button, Navbar, Typography} from "@material-tailwind/react";
 import NavList from "./NavList.jsx";
+import {useAuthStore} from "../stores/authStore.js";
 
 const Navigation = () => {
+
+    const {userInfo} = useAuthStore();
+
     return (
         <Navbar className="mx-auto max-w-screen-xl px-6 py-3 sticky top-0 z-10 backdrop-blur-md">
             <div className="flex items-center justify-between text-blue-gray-900">
@@ -18,14 +22,25 @@ const Navigation = () => {
                     <NavList />
                 </div>
 
-                <div className="hidden gap-2 lg:flex">
-                    <Button variant="text" size="sm" color="blue-gray">
-                        <a href="/login">Login</a>
-                    </Button>
-                    <Button variant="gradient" size="sm">
-                        <a href="/register">Register</a>
-                    </Button>
-                </div>
+                {userInfo ? (
+                    <div className="hidden gap-2 lg:flex">
+                        <Button variant="text" size="sm" color="blue-gray">
+                            Logout
+                        </Button>
+                        <Button variant="gradient" size="sm">
+                            <a href="/profile">Profile</a>
+                        </Button>
+                    </div>
+                ) : (
+                    <div className="hidden gap-2 lg:flex">
+                        <Button variant="text" size="sm" color="blue-gray">
+                            <a href="/login">Login</a>
+                        </Button>
+                        <Button variant="gradient" size="sm">
+                            <a href="/register">Register</a>
+                        </Button>
+                    </div>
+                )}
             </div>
         </Navbar>
     );
