8d9ab4dd3ecbfdd4db9de04d968189d1ce0a5a66
Pinto <pintokevin2002@hotmail.com>
2024-11-05 11:52:07 +0100

    Adding the new component to the PokeDetails.jsx

--- src/pages/PokeDetails.jsx
+++ src/pages/PokeDetails.jsx
@@ -1,6 +1,9 @@
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import {usePokemonStore} from "../stores/pokemonStore.js";
 import {useEffect} from "react";
+import {Button, Typography} from "@material-tailwind/react";
+import PokeDescription from "../components/PokeDescription.jsx";
+import PokeStats from "../components/PokeStats.jsx";
 
 const PokeDetails = () => {
 
@@ -13,9 +16,35 @@ const PokeDetails = () => {
     }, []);
 
     return (
-        <div>
-            <p>{pokeName}</p>
-        </div>
+        <section className="m-10 text-center">
+            <div className="py-16 px-40 rounded-xl bg-gradient-to-b from text-blue-gray-700 to black">
+                <div className="bg-white rounded-xl">
+                    {singlePokemon && (
+                        <>
+                            <Typography variant="h1">
+                                {singlePokemon?.pokemon_name}
+                            </Typography>
+                            <Typography variant="h2" className="text-gray-600">
+                                {singlePokemon?.pokemon_num}
+                            </Typography>
+                        </>
+                    )}
+                </div>
+                <div className="flex justify-center items-start gap-6 mt-10">
+                    <img className="w-1/4 bg-gray-300 rounded-xl" src={singlePokemon?.pokemon_infos.pokemon_img} alt={`${singlePokemon?.pokemon_name}'s picture`} />
+                    <PokeDescription pokemon={singlePokemon} />
+                </div>
+                <div className="mt-10">
+                    <PokeStats pokemon={singlePokemon} />
+                </div>
+                <div className="mt-10"></div>
+                <div className="mt-10 pb-10">
+                    <Button>
+                        <Link to={"/pokedex"}>More Pokemon</Link>
+                    </Button>
+                </div>
+            </div>
+        </section>
     );
 };
 
