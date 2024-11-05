bfa63ae80380c526f7ed38c8df530cc2f2ea0c20
Pinto <pintokevin2002@hotmail.com>
2024-11-05 10:30:56 +0100

    Creating the path to the new details page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer.jsx";
 import Redirect from "./pages/Redirect.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
+import PokeDetails from "./pages/PokeDetails.jsx";
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/details/:name" element={<PokeDetails />} />
               <Route path="/*" element={<Redirect />} />
           </Routes>
           <Footer />
