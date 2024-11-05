8cb771a14de72084c6f3411c8cefbf3db3a6ecd0
Pinto <pintokevin2002@hotmail.com>
2024-11-05 10:22:48 +0100

    Adding a getSinglePokemon method

--- src/stores/pokemonStore.js
+++ src/stores/pokemonStore.js
@@ -7,6 +7,7 @@ export const usePokemonStore = create((set) => ({
     error: null,
     success: false,
     randomPokemon: [],
+    singlePokemon: [],
 
     getRandomPokemon: async () => {
         set({loading: true, error: null});
@@ -14,6 +15,16 @@ export const usePokemonStore = create((set) => ({
             const response = await axios.get("http://localhost:3000/api/pokemon");
             const random = response.data.sort(() => Math.random() - Math.random()).slice(0, 3);
             set(() => ({randomPokemon: random, loading: false, success: true}));
+        } catch (error) {
+            set({error: error.message, loading: false});
+        }
+    },
+
+    getSinglePokemon: async (name) => {
+        set({loading: true, error: null});
+        try {
+            const response = await axios.get(`http://localhost:3000/api/pokemon/${name}`);
+            set(() => ({singlePokemon: response.data, loading: false, success: true}));
         } catch (error) {
             set({error: error.message, loading: false});
         }
