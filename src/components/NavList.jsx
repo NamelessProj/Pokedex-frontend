c7086798769b99bb5b5bb1023084344d9baefb6c
Kevin <pintokevin2002@hotmail.com>
2024-11-19 17:59:05 +0100

    Updating the NavList.jsx

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -1,26 +1,41 @@
 import {Typography} from "@material-tailwind/react";
+import {useAuthStore} from "../stores/authStore.js";
+import {Link} from "react-router-dom";
 
 const NavList = () => {
+    const {userInfo} = useAuthStore();
+
     return (
         <ul className="my-2 flex flex-col lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
             <Typography
-                as="a"
-                href="/"
                 variant="small"
                 color="blue-gray"
                 className="p-1 font-medium cursor-pointer"
             >
-                Home
+                <Link to="/">
+                    Home
+                </Link>
             </Typography>
             <Typography
-                as="a"
-                href="/"
                 variant="small"
                 color="blue-gray"
                 className="p-1 font-medium cursor-pointer"
             >
-                All Pokemon
+                <Link to="/pokedex">
+                    All Pokemon
+                </Link>
             </Typography>
+            {userInfo && (
+                <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="p-1 font-medium cursor-pointer"
+                >
+                    <Link to="/admin">
+                        Admin
+                    </Link>
+                </Typography>
+            )}
         </ul>
     );
 };
